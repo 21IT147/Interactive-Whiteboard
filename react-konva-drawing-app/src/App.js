93980e9bba48d3206fb4ea038567441d6db5c964
21IT147 <21IT147@charusat.edu.in>
2024-08-29 17:47:08 +0530

    some necessary changes

--- react-konva-drawing-app/src/App.js
+++ react-konva-drawing-app/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WhiteBoard from './components/Board/Whiteboard';
 import Home from './components/Home';
 import NoPage from './components/NoPage';
-import RoomManagement from './components/Room/RoomManagement';
 import CreateRoom from './components/Room/CreateRoom';
 import JoinRoom from './components/Room/JoinRoom';
 import '@mantine/core/styles.css';
@@ -16,7 +15,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/room/:roomId" element={<WhiteBoard />} />
         <Route path="/whiteboard" element={<WhiteBoard />} />
-        <Route path="/room-management" element={<RoomManagement />} />
         <Route path="/create-room" element={<CreateRoom />} />
         <Route path="/join-room" element={<JoinRoom />} />
         <Route path="*" element={<NoPage />} />
